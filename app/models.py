86dfc5dc036fe561113eead58ad303a412c1676d
Dorjee Lama <dorjeeglama@gmail.com>
2024-08-25 12:16:27 -0400

    fix import error

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ import sqlalchemy.orm as so
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-import hashlib import md5
+import hashlib as md5
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
